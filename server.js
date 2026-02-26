f64b8b027c842089b80a90bb3bdf4d3b56942515
Thuodev <samcreations104@gmail.com>
2026-02-27 00:22:18 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const ShortUrl = require('./models/shortUrls');
+const shortUrl = require('./models/shortUrls');
+
+mongoose.connect('mongodb://localhost/urlShortener');
+
+
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/', async(req, res) => {
+   const shortUrls = await shortUrl.find();
+  res.render('index', { shortUrls: shortUrls });
+});
+app.post('/shortUrls',async (req, res) => {
+   await shortUrl.create({ full: req.body.fullUrl })
+    res.redirect('/');
+  
+});
+app.get('/:shortUrl', async(req,res) =>{
+   const shortUrl= await ShortUrl.findOne({short:req.params.shortUrl})
+    if(shortUrl == null) return res.sendStatus(404);
+    shortUrl.clicks++;
+    shortUrl.save();
+    res.redirect(shortUrl.full);
+
+})
+app.listen(process.env.PORT ||5000);
